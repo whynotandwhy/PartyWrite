1c531fc25d19342bc9d318cdd645270f227d10ac
Ace Young <luadengames@gmail.com>
2020-07-04 21:00:18 +0200

    UI Prototype

---
+++ Assets/CoreUIElement.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CoreUIElement<T> : MonoBehaviour
+{
+    public abstract void UpdateUI(T newData);
+}
---
+++ Assets/IItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IItem
+{
+    string ItemName { get; }
+    Sprite ItemSprite { get; }
+    string ItemDescription { get; }
+    float RegalValue { get; }
+    float ExcitingValue { get; }
+    float HumorValue { get; }
+    float DifferentValue { get; }
+    float PlayerPrice { get; }
+    float CustomerPrice { get; }
+}
