8a9848a9aa7cc9c791d3f28ba7cdda9f6d317b28
Brian <whynotandwhy@gmail.com>
2020-07-09 18:59:51 -0400

    Setup of inventory stubs
    this is the setup for code and to decide how to handle specific scenarios specialization will occur within this section.

---
+++ Assets/Scripts/UI/Inventories/CoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// CoreManager should handle all common code logic 
+/// allowing all specialized behavore to be defined in extended classes
+/// </summary>
+public class CoreManger : DraggableManager<Draggable, IItem>
+{
+    public override void HandleHover(Draggable dropee, PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void HandleSlotDrop(PointerEventData eventData, Draggable dropped)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override Sprite SelectSprite(IItem item)
+    {
+        throw new System.NotImplementedException();
+    }
+}
